45a4f7f3bb2bc29b466abc73348c63d57a4afcd7
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-09-27 02:06:51 +0530

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class problem1{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int row = sc.nextInt();
+        for(int i =0;i<row;i++){
+            System.out.print("*");//entering the no of clumns manually
+            System.out.print("*");// NOW IN ANOTHER PROGRAM WE WILL PRINT THIS USING J
+            System.out.print("*");
+            System.out.print("*");
+            System.out.print("*");
+            System.out.println();
+        
+        
+        }
+        
+    }
+}
