bd00a1e20bdfcfa713d47ee3390a750d475b831e
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-09-28 01:34:21 +0530

    Add files via upload

---
+++ problem12.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class problem12 {
+        public static void main(String[] args) {
+              
+          
+    
+    Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the value of row:");
+      int n = sc.nextInt();
+        System.out.println("enter the value of column");//pehle outpput
+        int s= sc.nextInt();
+        int a = 1;
+    
+      for(int i = 1;i<=n;i++){
+           
+           for(int j =1;j<=i ; j++){
+        
+            System.out.print(a++ + "");
+
+         }
+         System.out.println();
+        }
+        //System.out.println();
+}
+        
+
+    } 
+
+
+    
+
+
+
+
+    
+
+
+
+
+    
+
