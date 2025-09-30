c8612fd1e45f5ebe02d87d1126b1187ece0b6a7c
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-09-30 22:02:59 +0530

    Add files via upload

---
+++ problem18.java
@@ -0,0 +1,235 @@
+  
+
+
+//   import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//         System.out.println("Enter the Column no");
+//         int c= sc.nextInt();
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=r+1-i;j++){
+//                 System.out.print(" " + "");
+
+//             }
+//             for(int j =1;j<=i;j++){
+//                 System.out.print((char)(i+96) + "");//NOTE IF WRITTEN J + THEN WILL PRINT ABCD IF I AA BB
+                
+//             }
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+
+
+// //   
+//  import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//         System.out.println("Enter the Column no");
+//         int c= sc.nextInt();
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=r+1-i;j++){
+//                 System.out.print(" " + "");
+
+//             }
+//             for(int j =1;j<=i;j++){
+//                 System.out.print(j + "");
+//             }
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+
+
+
+//    import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//         System.out.println("Enter the Column no");
+//         int c= sc.nextInt();
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 2;j<=r+1-i;j++){
+//                 System.out.print("* " + "");
+
+//             }
+//             for(int j =1;j<=i;j++){
+//                 System.out.print(j + "");
+//             }
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+
+
+ 
+//   import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//         System.out.println("Enter the Column no");
+//         int c= sc.nextInt();
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=r-i;j++){//WILL PRINT triangle 
+//                 System.out.print(" " + "");// WILL PRINT triangle 
+
+//             }
+//             for(int j =1;j<=2*i-1;j++){// WILL PRINT pyramid
+//                 System.out.print( "*" + "");//NOTE IF WRITTEN J + THEN WILL PRINT ABCD IF I AA BB
+                
+//             }
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+
+
+//   import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//      //   System.out.println("Enter the Column no");
+//        // int c= sc.nextInt();
+//         int nsp = r-1;
+//         int nst = 1;
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=nsp;j++){//WILL PRINT triangle 
+//                 System.out.print("$" + "");// WILL PRINT triangle 
+
+//             }
+//             for(int j =1;j<=nst;j++){// WILL PRINT pyramid
+//                 System.out.print( "*" + "");//NOTE IF WRITTEN J + THEN WILL PRINT ABCD IF I AA BB
+                
+//             }nsp --;
+//             nst += 2;
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+
+  
+//   
+// import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//      //   System.out.println("Enter the Column no");
+//        // int c= sc.nextInt();
+//         int nsp = r-1;
+//         int nst = 1;
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=nsp;j++){//WILL PRINT triangle 
+//                 System.out.print(" " + "");// WILL PRINT triangle 
+
+//             }
+//             for(int j =1;j<=nst;j++){// WILL PRINT pyramid
+//                 System.out.print( j  + "");//NOTE IF WRITTEN J + THEN WILL PRINT ABCD IF I AA BB
+                
+//             }nsp --;
+//             nst += 2;
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+
+// import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r= sc.nextInt();
+//      //   System.out.println("Enter the Column no");
+//        // int c= sc.nextInt();
+//         int nsp = r-1;
+//         int nst = 1;
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=nsp;j++){//WILL PRINT triangle 
+//                 System.out.print(" " + "");// WILL PRINT triangle 
+
+//             }
+//             for(int j =1;j<=nst;j++){// WILL PRINT pyramid
+//                 System.out.print( i  + "");//NOTE IF WRITTEN J + THEN WILL PRINT ABCD IF I AA BB
+                
+//             }nsp --;
+//             nst += 2;
+//             System.out.println();
+//         }
+    
+
+
+//     }
+
+// }
+//pyramid PRINTING
+
+// import java.util.Scanner ;
+//    public class problem18{
+
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row no");
+//         int r = sc.nextInt();
+//        System.out.println("Enter the Column no");
+//        // int c= sc.nextInt();
+//         int nsp = r-1;
+//         int nst = 1;
+//         for(int i = 1 ;i<=r;i++){
+//             for(int j = 1;j<=nsp;j++){
+//                 System.out.print(" " + ""); 
+
+//             }
+//             for(int j =1;j<=nst;j++){// WILL PRINT pyramid
+//                 System.out.print( "*"  + "");
+                
+//             }nsp --;
+//             nst += 2;
+//             System.out.println();
+//         }
+//     }
+// }
+    
